fd304e1a63088342da4fdec2d8a0eb52e5e1a247
Willen Thiago <willenthiagoc@gmail.com>
2024-01-13 17:16:22 -0300

    test (funções)
    Testes de implementação das funções básicas da calculadora

--- js/script.js
+++ js/script.js
@@ -5,15 +5,23 @@
  *      Subtração
  *      Multiplicação
  *      Divisão
- * 
+ *      Resultado
  */
 
-let dis = document.getElementById('idNum')
-let btn = document.querySelector('div#idBtn')
+let num = document.querySelector('#idNum')
+let btn = document.getElementsByClassName('.btn')
+let calc = []
 
-
-btn.addEventListener("click", operacao)
-
-function operacao(){
-    window.alert(btn.value)
+function operador(){
+    if(num.value == 0){
+        alert('Insire um número para começar')
+    } else {
+        calc.push(Number(num.value))
+        let n = calc[0]
+        for(let pos in calc){
+            n = calc[pos]
+        }
+        let op = new btn()
+        let opA = op.value
+    }
 }
