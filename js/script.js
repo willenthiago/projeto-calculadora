c8144500602abb6a90fd60f2012d3d4be906d3d1
Willen Thiago <willenthiagoc@gmail.com>
2024-01-30 19:48:09 -0300

    fix (encapsulamento e estilo)

--- js/script.js
+++ js/script.js
@@ -5,23 +5,15 @@
  *      Subtração
  *      Multiplicação
  *      Divisão
+ *      Porcentagem
  *      Resultado
  */
 
-let num = document.querySelector('#idNum')
-let btn = document.getElementsByClassName('.btn')
-let calc = []
-
-function operador(){
-    if(num.value == 0){
-        alert('Insire um número para começar')
-    } else {
-        calc.push(Number(num.value))
-        let n = calc[0]
-        for(let pos in calc){
-            n = calc[pos]
-        }
-        let op = new btn()
-        let opA = op.value
-    }
+let dis = document.getElementById('input-display');
+let btn = document.getElementById('buttons');
+let num = 0;
+function placeHolder(){
+ dis.placeholder = '0';
 }
+/*
+*/
